c2f800a471af1707d1e8c44d6c2e571f35abbd46
Aso <53526710+AsoDesu@users.noreply.github.com>
2023-02-09 18:04:15 +0000

    Fix some discord stuff

---
+++ src/main/java/net/asodev/islandutils/mixins/BossHeathAccessor.java
@@ -0,0 +1,17 @@
+package net.asodev.islandutils.mixins;
+
+import net.minecraft.client.gui.components.BossHealthOverlay;
+import net.minecraft.client.gui.components.LerpingBossEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Mixin(BossHealthOverlay.class)
+public interface BossHeathAccessor {
+
+    @Accessor
+    Map<UUID, LerpingBossEvent> getEvents();
+
+}
