1e872108c103f24148a27d7fc0a94f8deacff9cd
Aso <53526710+AsoDesu@users.noreply.github.com>
2023-07-06 10:40:22 +0100

    Fix bug where disconnecting from *anything* would stop presence

--- src/main/java/net/asodev/islandutils/mixins/discord/ConnectionMixin.java
+++ src/main/java/net/asodev/islandutils/mixins/discord/ConnectionMixin.java
@@ -6,19 +6,27 @@ import net.asodev.islandutils.state.MccIslandState;
 import net.minecraft.network.Connection;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
 @Mixin(Connection.class)
-public class ConnectionMixin {
+public abstract class ConnectionMixin {
+
+    @Shadow public abstract SocketAddress getRemoteAddress();
 
     @Inject(method = "disconnect", at = @At("HEAD"))
     private void disconnect(Component component, CallbackInfo ci) {
-        DiscordPresenceUpdator.started = null;
-        DiscordPresenceUpdator.clear();
-
-        MccIslandState.setGame(GAME.HUB);
+        String hostName = ((InetSocketAddress) getRemoteAddress()).getHostName();
+        if (hostName.contains("mccisland.net")) {
+            DiscordPresenceUpdator.started = null;
+            DiscordPresenceUpdator.clear();
+            MccIslandState.setGame(GAME.HUB);
+        }
     }
 
 }
