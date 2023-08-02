29f539dc151df5942d05259e1abf27a35f8c44c9
Aso <53526710+AsoDesu@users.noreply.github.com>
2023-08-02 20:32:27 +0100

    refactor: finish options implementation

--- src/main/java/net/asodev/islandutils/options/categories/SplitsCategory.java
+++ src/main/java/net/asodev/islandutils/options/categories/SplitsCategory.java
@@ -1,38 +1,75 @@
 package net.asodev.islandutils.options.categories;
 
 import dev.isxander.yacl3.api.ConfigCategory;
+import dev.isxander.yacl3.api.Option;
+import dev.isxander.yacl3.api.OptionDescription;
+import dev.isxander.yacl3.api.controller.EnumControllerBuilder;
+import dev.isxander.yacl3.api.controller.TickBoxControllerBuilder;
 import net.asodev.islandutils.state.splits.SplitType;
+import net.minecraft.network.chat.Component;
 
 public class SplitsCategory implements OptionsCategory {
-
+    private static final SplitsCategory defaults = new SplitsCategory();
     boolean enablePkwSplits = false;
     boolean sendSplitTime = true;
     boolean showTimer = true;
     boolean showSplitImprovements = true;
     SplitType saveMode = SplitType.BEST;
 
+    @Override
+    public ConfigCategory getCategory() {
+        Option<Boolean> enableOption = Option.<Boolean>createBuilder()
+                .name(Component.translatable("text.autoconfig.islandutils.option.enablePkwSplits"))
+                .controller(TickBoxControllerBuilder::create)
+                .binding(defaults.enablePkwSplits, () -> enablePkwSplits, value -> this.enablePkwSplits = value)
+                .build();
+        Option<Boolean> sendOption = Option.<Boolean>createBuilder()
+                .name(Component.translatable("text.autoconfig.islandutils.option.sendSplitTime"))
+                .description(OptionDescription.of(Component.translatable("text.autoconfig.islandutils.option.sendSplitTime.@Tooltip")))
+                .controller(TickBoxControllerBuilder::create)
+                .binding(defaults.sendSplitTime, () -> sendSplitTime, value -> this.sendSplitTime = value)
+                .build();
+        Option<Boolean> showOption = Option.<Boolean>createBuilder()
+                .name(Component.translatable("text.autoconfig.islandutils.option.showTimer"))
+                .description(OptionDescription.of(Component.translatable("text.autoconfig.islandutils.option.showTimer.@Tooltip")))
+                .controller(TickBoxControllerBuilder::create)
+                .binding(defaults.showTimer, () -> showTimer, value -> this.showTimer = value)
+                .build();
+        Option<Boolean> showImprovesOption = Option.<Boolean>createBuilder()
+                .name(Component.translatable("text.autoconfig.islandutils.option.showSplitImprovements"))
+                .description(OptionDescription.of(Component.translatable("text.autoconfig.islandutils.option.showSplitImprovements.@Tooltip")))
+                .controller(TickBoxControllerBuilder::create)
+                .binding(defaults.showSplitImprovements, () -> showSplitImprovements, value -> this.showSplitImprovements = value)
+                .build();
+        Option<SplitType> saveOption = Option.<SplitType>createBuilder()
+                .name(Component.translatable("text.autoconfig.islandutils.option.saveMode"))
+                .description(OptionDescription.of(Component.translatable("text.autoconfig.islandutils.option.saveMode.@Tooltip")))
+                .controller((opt) -> EnumControllerBuilder.create(opt).enumClass(SplitType.class))
+                .binding(defaults.saveMode, () -> saveMode, value -> this.saveMode = value)
+                .build();
+        return ConfigCategory.createBuilder()
+                .name(Component.literal("Parkour Warrior Splits"))
+                .option(enableOption)
+                .option(sendOption)
+                .option(showOption)
+                .option(showImprovesOption)
+                .option(saveOption)
+                .build();
+    }
+
     public boolean isEnablePkwSplits() {
         return enablePkwSplits;
     }
-
     public boolean isSendSplitTime() {
-        return sendSplitTime;
+        return enablePkwSplits && sendSplitTime;
     }
-
     public boolean isShowTimer() {
-        return showTimer;
+        return enablePkwSplits && showTimer;
     }
-
     public boolean isShowSplitImprovements() {
-        return showSplitImprovements;
+        return enablePkwSplits && showSplitImprovements;
     }
-
     public SplitType getSaveMode() {
         return saveMode;
-    }
-
-    @Override
-    public ConfigCategory getCategory() {
-        return null;
     }
 }
--- src/main/java/net/asodev/islandutils/state/splits/SplitType.java
+++ src/main/java/net/asodev/islandutils/state/splits/SplitType.java
@@ -1,6 +1,15 @@
 package net.asodev.islandutils.state.splits;
 
-public enum SplitType {
+import dev.isxander.yacl3.api.NameableEnum;
+import net.minecraft.network.chat.Component;
+
+public enum SplitType implements NameableEnum {
     BEST,
-    AVG
+    AVG;
+
+
+    @Override
+    public Component getDisplayName() {
+        return Component.translatable("islandutils.splittype." + this.name().toLowerCase());
+    }
 }
