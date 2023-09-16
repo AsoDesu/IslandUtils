e2bfeab9e033719175ca4e651ca63fec6ed1b891
Aso <53526710+AsoDesu@users.noreply.github.com>
2023-09-16 13:15:14 +0100

    feat: add slot number to mcci tooltip debug

--- src/main/java/net/asodev/islandutils/mixins/ItemIDMixin.java
+++ src/main/java/net/asodev/islandutils/mixins/ItemIDMixin.java
@@ -1,22 +1,28 @@
 package net.asodev.islandutils.mixins;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import net.asodev.islandutils.mixins.accessors.ContainerScreenAccessor;
 import net.asodev.islandutils.options.IslandOptions;
 import net.asodev.islandutils.state.MccIslandState;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ChestMenu;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -41,13 +47,35 @@ public abstract class ItemIDMixin {
     private void addTooltipLines(@Nullable Player player, TooltipFlag tooltipFlag, CallbackInfoReturnable<List<Component>> cir, List<Component> list) {
         if (!MccIslandState.isOnline() || !IslandOptions.getMisc().isDebugMode()) return;
         if (!InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), InputConstants.KEY_LCONTROL)) return;
-        if (!this.hasTag()) return;
 
+        MutableComponent toAppend = Component.empty();
+        if (this.hasTag()) tryAddCustomItemID(toAppend); // Append MCCI Custom Item ID
+        if (Minecraft.getInstance().screen != null) tryAddSlotNumber(toAppend); // Append Slot Number
+
+        if (!toAppend.getSiblings().isEmpty()) { // Add to tooltip lines if not empty
+            list.add(toAppend);
+        }
+    }
+
+    @Unique
+    private void tryAddCustomItemID(MutableComponent toAppend) {
         CompoundTag publicBukkitValues = this.getTagElement("PublicBukkitValues");
         if (publicBukkitValues == null) return;
         String customItemId = publicBukkitValues.getString("mcc:custom_item_id");
         if (customItemId.equals("")) return;
-        list.add(Component.literal(customItemId).withStyle(ChatFormatting.AQUA));
+        toAppend.append(Component.literal(customItemId).withStyle(ChatFormatting.AQUA));
+    }
+
+    private void tryAddSlotNumber(MutableComponent toAppend) {
+        ItemStack me = (ItemStack)(Object)this;
+        Screen screen = Minecraft.getInstance().screen;
+        if (!(screen instanceof AbstractContainerScreen<?> containerScreen)) return;
+        Slot hoveredSlot = ((ContainerScreenAccessor) containerScreen).getHoveredSlot();
+
+        if (!hoveredSlot.hasItem()) return;
+        if (hoveredSlot.getItem() != me) return;
+        Component component = Component.literal(" (" + hoveredSlot.getContainerSlot() + ")").withStyle(ChatFormatting.GRAY);
+        toAppend.append(component);
     }
 
 }
