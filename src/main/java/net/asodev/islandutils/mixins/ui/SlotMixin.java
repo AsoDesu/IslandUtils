06e437b0338710a6ef853fea2edf9178e953ad7b
TheMysterys <the-mystery@outlook.com>
2023-10-11 00:49:19 +1300

    Added preventing empty slots from highlighting.
    Also prevents blank items from highlighting too.

---
+++ src/main/java/net/asodev/islandutils/mixins/ui/SlotMixin.java
@@ -0,0 +1,35 @@
+package net.asodev.islandutils.mixins.ui;
+
+import net.asodev.islandutils.state.Game;
+import net.asodev.islandutils.state.MccIslandState;
+import net.asodev.islandutils.util.Utils;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Slot.class)
+public abstract class SlotMixin {
+
+    @Shadow
+    public abstract boolean hasItem();
+
+    @Shadow
+    public abstract ItemStack getItem();
+
+    @Inject(method = "isHighlightable", at = @At("HEAD"), cancellable = true)
+    private void isHighlightable(CallbackInfoReturnable<Boolean> cir) {
+        if (MccIslandState.isOnline() && MccIslandState.getGame().equals(Game.HUB)) {
+            ResourceLocation itemID = Utils.getCustomItemID(this.getItem());
+            if (itemID == null) {
+                cir.setReturnValue(this.hasItem());
+            } else if (itemID.getPath().equals("island_interface.generic.blank")) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
+}
