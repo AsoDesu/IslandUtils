8b30a9f1336beb38e90cb634fb7de7d5007139b6
Aso <53526710+AsoDesu@users.noreply.github.com>
2024-04-20 18:15:29 +0100

    fix: remove crafting items in non-input slots

--- src/main/java/net/asodev/islandutils/modules/crafting/CraftingUI.java
+++ src/main/java/net/asodev/islandutils/modules/crafting/CraftingUI.java
@@ -31,7 +31,10 @@ public class CraftingUI {
 
     private static TextColor timeLeftColor = ChatUtils.parseColor("#FF5556");
     public static void analyseCraftingItem(CraftingMenuType type, ItemStack item, int slot) {
-        if (!isInputSlot(slot)) return;
+        if (!isInputSlot(slot)) {
+            CraftingItems.removeSlot(type, slot);
+            return;
+        }
 
         List<Component> lores = Utils.getLores(item);
         if (lores != null && isActive(lores)) {
--- src/main/java/net/asodev/islandutils/modules/crafting/state/CraftingItems.java
+++ src/main/java/net/asodev/islandutils/modules/crafting/state/CraftingItems.java
@@ -45,8 +45,8 @@ public class CraftingItems {
      * @param slot The slot you wish to remove
      */
     public static void removeSlot(CraftingMenuType type, int slot) {
-        items.removeIf(i -> i.getCraftingMenuType() == type && i.getSlot() == slot);
-        save();
+        boolean wasRemoved = items.removeIf(i -> i.getCraftingMenuType() == type && i.getSlot() == slot);
+        if (wasRemoved) save();
     }
 
     public static <T> void submit(Runnable task) {
@@ -84,7 +84,7 @@ public class CraftingItems {
                 try {
                     array.add(item.toJson());
                 } catch (Exception e) {
-                    logger.error("Failed to save item: " + item, e);
+                    logger.error("Failed to save item: {}", item, e);
                 }
             }
 
