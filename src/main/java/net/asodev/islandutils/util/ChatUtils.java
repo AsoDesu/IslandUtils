8ab31ad50f9805408f577f93572720563776a25c
Aso <53526710+AsoDesu@users.noreply.github.com>
2024-04-20 18:04:36 +0100

    fix: log into the log file if debug mode is disabled

--- src/main/java/net/asodev/islandutils/util/ChatUtils.java
+++ src/main/java/net/asodev/islandutils/util/ChatUtils.java
@@ -7,11 +7,13 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.TextColor;
 import net.minecraft.resources.ResourceLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 
 public class ChatUtils {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger("IslandUtils");
     public static final Style iconsFontStyle = Style.EMPTY.withColor(ChatFormatting.WHITE).withFont(new ResourceLocation("island","icons"));
     public static final String prefix = "&b[&eIslandUtils&b]";
     public static String translate(String s) {
@@ -27,13 +29,11 @@ public class ChatUtils {
     }
 
     public static void debug(String s) {
-        if (!IslandOptions.getMisc().isDebugMode()) return;
-        send(Component.literal(translate("&7[IslandUtils] " + s)));
-    }
-
-    public static void debug(Component component) {
-        if (!IslandOptions.getMisc().isDebugMode()) return;
-        send(Component.literal(translate("&7[IslandUtils] ")).append(component));
+        if (!IslandOptions.getMisc().isDebugMode()) {
+            LOGGER.info("[DEBUG] {}", s);
+            return;
+        }
+        send(Component.literal("[IslandUtils] " + s).withStyle(ChatFormatting.GRAY));
     }
 
     public static void send(Component component) {
