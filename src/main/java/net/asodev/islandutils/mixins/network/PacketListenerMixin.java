5a1aeb5ab8956f6119b4af8dc57c1dd09f796e74
Hamish Lester <89168856+thehamish555@users.noreply.github.com>
2025-01-11 16:41:51 +1300

    Update PacketListenerMixin.java

--- src/main/java/net/asodev/islandutils/mixins/network/PacketListenerMixin.java
+++ src/main/java/net/asodev/islandutils/mixins/network/PacketListenerMixin.java
@@ -11,6 +11,7 @@ import net.asodev.islandutils.modules.cosmetics.CosmeticSlot;
 import net.asodev.islandutils.modules.cosmetics.CosmeticState;
 import net.asodev.islandutils.modules.music.MusicManager;
 import net.asodev.islandutils.modules.splits.LevelTimer;
+import net.asodev.islandutils.modules.splits.SplitManager;
 import net.asodev.islandutils.options.IslandOptions;
 import net.asodev.islandutils.state.Game;
 import net.asodev.islandutils.state.MccIslandState;
@@ -59,31 +60,40 @@ public abstract class PacketListenerMixin extends ClientCommonPacketListenerImpl
 
     // I should really separate these mixins...
 
-    @Shadow private CommandDispatcher<CommandSourceStack> commands;
+    @Shadow
+    private CommandDispatcher<CommandSourceStack> commands;
 
-    protected PacketListenerMixin(Minecraft minecraft, Connection connection, CommonListenerCookie commonListenerCookie) {
+    protected PacketListenerMixin(Minecraft minecraft, Connection connection,
+            CommonListenerCookie commonListenerCookie) {
         super(minecraft, connection, commonListenerCookie);
     }
 
     @Inject(method = "handleSoundEvent", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/PacketUtils;ensureRunningOnSameThread(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;Lnet/minecraft/util/thread/BlockableEventLoop;)V", shift = At.Shift.AFTER), cancellable = true)
     public void handleCustomSoundEvent(ClientboundSoundPacket clientboundCustomSoundPacket, CallbackInfo ci) {
-        if (!MccIslandState.isOnline()) return;
+        if (!MccIslandState.isOnline())
+            return;
 
         ResourceLocation soundLoc = clientboundCustomSoundPacket.getSound().value().getLocation();
-        if (!soundLoc.getNamespace().equals("mcc")) return;
+        if (!soundLoc.getNamespace().equals("mcc"))
+            return;
 
         if (soundLoc.getPath().startsWith("music.")) {
             ChatUtils.debug("Starting music " + soundLoc);
             MusicManager.onMusicSoundPacket(clientboundCustomSoundPacket, this.minecraft);
             ci.cancel();
         }
+        if (MccIslandState.getGame() == Game.PARKOUR_WARRIOR_DOJO) {
+            LevelTimer.onSound(clientboundCustomSoundPacket);
+        }
     }
 
     @Inject(method = "handleStopSoundEvent", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/PacketUtils;ensureRunningOnSameThread(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;Lnet/minecraft/util/thread/BlockableEventLoop;)V", shift = At.Shift.AFTER), cancellable = true)
     public void handleStopSoundEvent(ClientboundStopSoundPacket clientboundStopSoundPacket, CallbackInfo ci) {
-        if (!MccIslandState.isOnline()) return;
+        if (!MccIslandState.isOnline())
+            return;
         ResourceLocation soundLoc = clientboundStopSoundPacket.getName();
-        if (soundLoc == null || !soundLoc.getNamespace().equals("mcc")) return;
+        if (soundLoc == null || !soundLoc.getNamespace().equals("mcc"))
+            return;
 
         if (soundLoc.getPath().startsWith("music.")) {
             ChatUtils.debug("Stopping music " + soundLoc);
@@ -92,45 +102,60 @@ public abstract class PacketListenerMixin extends ClientCommonPacketListenerImpl
         }
     }
 
-    @Inject(method = "handleContainerContent", at = @At("TAIL")) // Cosmetic previews, whenever we get our cosmetics back after closing menu
-    private void containerContent(ClientboundContainerSetContentPacket clientboundContainerSetContentPacket, CallbackInfo ci) {
-        if (!MccIslandState.isOnline()) return;
+    @Inject(method = "handleContainerContent", at = @At("TAIL")) // Cosmetic previews, whenever we get our cosmetics
+                                                                 // back after closing menu
+    private void containerContent(ClientboundContainerSetContentPacket clientboundContainerSetContentPacket,
+            CallbackInfo ci) {
+        if (!MccIslandState.isOnline())
+            return;
         Player player = Minecraft.getInstance().player;
-        if (player == null) return; // If no player, stop
-        if (clientboundContainerSetContentPacket.getContainerId() != 0) return; // If this is a chest, stop
+        if (player == null)
+            return; // If no player, stop
+        if (clientboundContainerSetContentPacket.getContainerId() != 0)
+            return; // If this is a chest, stop
 
         CosmeticState.hatSlot.setOriginal(new CosmeticSlot(player.getInventory().armor.get(3)));
         CosmeticState.accessorySlot.setOriginal(new CosmeticSlot(player.getInventory().offhand.get(0)));
     }
 
     private static Pattern timerPattern = Pattern.compile("(\\d+:\\d+)");
+
     @Inject(method = "handleBossUpdate", at = @At("HEAD")) // Discord presence, time left
     private void handleBossUpdate(ClientboundBossEventPacket clientboundBossEventPacket, CallbackInfo ci) {
-        if (!MccIslandState.isOnline()) return;
+        if (!MccIslandState.isOnline())
+            return;
         // Create a handler for the bossbar
-        ClientboundBossEventPacket.Handler bossbarHandler = new ClientboundBossEventPacket.Handler(){
+        ClientboundBossEventPacket.Handler bossbarHandler = new ClientboundBossEventPacket.Handler() {
             @Override
             public void updateName(UUID uUID, Component component) {
-                if (!component.getString().contains(":")) return; // If we don't have a timer, move on with our lives
+                if (!component.getString().contains(":"))
+                    return; // If we don't have a timer, move on with our lives
                 try {
                     String text = component.getString();
                     Matcher matcher = timerPattern.matcher(text);
-                    if (!matcher.find()) return;
+                    if (!matcher.find())
+                        return;
                     String timer = matcher.group(1);
                     String[] split = timer.split(":"); // Split by the timer
                     String minsText = split[0]; // Get the left side
                     String secsText = split[1]; // Get the right side
 
-                    int mins = Integer.parseInt( minsText.substring( Math.max(minsText.length() - 2, 0)) ); // Get the last 2 character of the left side
-                    int secs = Integer.parseInt( secsText.substring(0, 2) ); // Get the first 2 on the right side
+                    int mins = Integer.parseInt(minsText.substring(Math.max(minsText.length() - 2, 0))); // Get the last
+                                                                                                         // 2 character
+                                                                                                         // of the left
+                                                                                                         // side
+                    int secs = Integer.parseInt(secsText.substring(0, 2)); // Get the first 2 on the right side
 
-                    long secondsLeft = ((mins * 60L) + secs+1);
-                    long finalUnix = System.currentTimeMillis() + (secondsLeft * 1000); // Get the timestamp when the game will end
+                    long secondsLeft = ((mins * 60L) + secs + 1);
+                    long finalUnix = System.currentTimeMillis() + (secondsLeft * 1000); // Get the timestamp when the
+                                                                                        // game will end
 
                     DiscordPresenceUpdator.timeLeftBossbar = uUID; // why do i do this again?
                     DiscordPresenceUpdator.updateTimeLeft(finalUnix); // Update our time left!!
-                } catch (Exception ignored) {}
+                } catch (Exception ignored) {
+                }
             }
+
             @Override
             public void remove(UUID uUID) { // tbf, i don't this is ever called, but y'know, just to be sure
                 if (DiscordPresenceUpdator.timeLeftBossbar == uUID)
@@ -141,8 +166,10 @@ public abstract class PacketListenerMixin extends ClientCommonPacketListenerImpl
     }
 
     @Inject(method = "handleCommandSuggestions", cancellable = true, at = @At("HEAD")) // "Friends in this game: "
-    private void commandSuggestionsResponse(ClientboundCommandSuggestionsPacket clientboundCommandSuggestionsPacket, CallbackInfo ci) {
-        if (clientboundCommandSuggestionsPacket.id() == TRANSACTION_ID) { // If we get back suggestions from our previous request
+    private void commandSuggestionsResponse(ClientboundCommandSuggestionsPacket clientboundCommandSuggestionsPacket,
+            CallbackInfo ci) {
+        if (clientboundCommandSuggestionsPacket.id() == TRANSACTION_ID) { // If we get back suggestions from our
+                                                                          // previous request
             ci.cancel(); // Stop minecraft... please.
             List<String> friends = clientboundCommandSuggestionsPacket // our friends suggestions
                     .suggestions() // the suggestions
@@ -154,31 +181,37 @@ public abstract class PacketListenerMixin extends ClientCommonPacketListenerImpl
 
     TextColor textColor = ChatUtils.parseColor("#FFA800"); // Trap Title Text Color
     Style style = Style.EMPTY.withColor(textColor); // Style for the trap color
+
     @Inject(method = "setSubtitleText", at = @At("HEAD"), cancellable = true)
     private void titleText(ClientboundSetSubtitleTextPacket clientboundSetSubtitleTextPacket, CallbackInfo ci) {
         if (MccIslandState.getGame() == Game.HITW) {
             ClassicAnnouncer.handleTrap(clientboundSetSubtitleTextPacket, ci);
         } else if (MccIslandState.getGame() == Game.PARKOUR_WARRIOR_DOJO) {
             LevelTimer instance = LevelTimer.getInstance();
-            if (instance == null) return;
+            if (instance == null)
+                return;
             instance.handleSubtitle(clientboundSetSubtitleTextPacket, ci);
         }
     }
 
     @Inject(method = "setTitleText", at = @At("HEAD")) // Game Over Sound Effect
     private void gameOver(ClientboundSetTitleTextPacket clientboundSetTitleTextPacket, CallbackInfo ci) {
-        if (MccIslandState.getGame() != Game.HITW) return; // Make sure we're playing HITW
-        if (!IslandOptions.getClassicHITW().isClassicHITW()) return; // Requires isClassicHITW
+        if (MccIslandState.getGame() != Game.HITW)
+            return; // Make sure we're playing HITW
+        if (!IslandOptions.getClassicHITW().isClassicHITW())
+            return; // Requires isClassicHITW
         String title = clientboundSetTitleTextPacket.text().getString().toUpperCase(); // Get the title in upper case
 
         if (title.contains("GAME OVER")) { // If we got game over title, play sound
-            Minecraft.getInstance().getSoundManager().play(SimpleSoundInstance.forUI(IslandSoundEvents.ANNOUNCER_GAME_OVER, 1f)); // Play the sound!!
+            Minecraft.getInstance().getSoundManager()
+                    .play(SimpleSoundInstance.forUI(IslandSoundEvents.ANNOUNCER_GAME_OVER, 1f)); // Play the sound!!
         }
     }
 
     @Inject(method = "handleSystemChat", at = @At("HEAD"), cancellable = true)
     private void onChat(ClientboundSystemChatPacket clientboundSystemChatPacket, CallbackInfo ci) {
-        if (clientboundSystemChatPacket.overlay() || !MccIslandState.isOnline()) return;
+        if (clientboundSystemChatPacket.overlay() || !MccIslandState.isOnline())
+            return;
 
         IslandUtilsEvents.Modifier<Component> modifier = new IslandUtilsEvents.Modifier<>();
         IslandUtilsEvents.CHAT_MESSAGE.invoker().onChatMessage(clientboundSystemChatPacket, modifier);
