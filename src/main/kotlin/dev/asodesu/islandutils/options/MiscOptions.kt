276dded50de199b74925efa6c03238dcfc3ccea9
Aso <53526710+AsoDesu@users.noreply.github.com>
2025-05-27 14:40:22 +0100

    feat: add confirm disconnect screen

--- src/main/kotlin/dev/asodesu/islandutils/options/MiscOptions.kt
+++ src/main/kotlin/dev/asodesu/islandutils/options/MiscOptions.kt
@@ -8,6 +8,7 @@ object MiscOptions : ConfigSection("section.misc") {
     val scavengingTotals = toggle("chest_scavenging_totals", desc = true, def = true)
     val remnantHighlight = toggle("chest_remnant_highlight", desc = true, def = true)
     val fishingUpgrades = toggle("chest_fishing_upgrade", desc = true, def = true)
+    val confirmDisconnect = toggle("confirm_disconnect", desc = true, def = true)
 
     object Debug : ConfigGroup("section.misc.debug") {
         val debugMode = toggle(name = "debug_mode", desc = true, def = false)
