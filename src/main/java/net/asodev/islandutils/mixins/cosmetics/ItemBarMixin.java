ffc70f90a0096ea3d3ffcbd71e756f16244210c2
violine1101 <violine1101@gmail.com>
2025-08-23 12:18:48 +0200

    Add chroma bar to cosmetics

---
+++ src/main/java/net/asodev/islandutils/mixins/cosmetics/ItemBarMixin.java
@@ -0,0 +1,140 @@
+package net.asodev.islandutils.mixins.cosmetics;
+
+import net.asodev.islandutils.modules.cosmetics.CosmeticChroma;
+import net.asodev.islandutils.options.IslandOptions;
+import net.asodev.islandutils.util.Utils;
+import net.minecraft.util.ARGB;
+import net.minecraft.util.Mth;
+import net.minecraft.world.item.ItemStack;
+import org.apache.commons.lang3.math.Fraction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+@Mixin(ItemStack.class)
+public class ItemBarMixin {
+    @Unique
+    private static final Pattern REPUTATION_REGEX = Pattern.compile("^Royal Donations: (\\d+)/(\\d+)$");
+
+    @Unique
+    private static final String CHROMA_LABEL = "Chromas Unlocked:";
+
+    @Unique
+    private static final Pattern CHROMA_REGEX = Pattern.compile("^([\\uE02A\\uE02E\\uE02F\\uE02C\\uE02D\\uE02B]{5}) - (\\d+)\\uE328$");
+
+    @Unique
+    private static final int REPUTATION_BAR_COLOR = ARGB.colorFromFloat(1.0F, 0.66F, 0.33F, 1.0F);
+
+    @Unique
+    private static final int CHROMA_BAR_COLOR_SPEED = 2000;
+
+    @Unique
+    private Optional<Fraction> getReputation() {
+        var itemStack = (ItemStack)(Object)this;
+        var lore = Utils.getLores(itemStack);
+        if (lore == null) return Optional.empty();
+
+        return lore.stream().<Optional<Fraction>>map(line -> {
+            var matcher = REPUTATION_REGEX.matcher(line.getString());
+            if (!matcher.find()) return Optional.empty();
+
+            var level = Integer.parseInt(matcher.group(1));
+            var max = Integer.parseInt(matcher.group(2));
+
+            try {
+                var fraction = Fraction.getFraction(level, max);
+                return Optional.of(fraction);
+            } catch (ArithmeticException e) {
+                return Optional.empty();
+            }
+        }).filter(Optional::isPresent).map(Optional::get).findFirst();
+    }
+
+    @Unique
+    private Optional<List<CosmeticChroma>> getChromas() {
+        var itemStack = (ItemStack)(Object)this;
+        var lore = Utils.getLores(itemStack);
+        if (lore == null) return Optional.empty();
+
+        if (lore.stream().noneMatch(it -> it.getString().equals(CHROMA_LABEL))) return Optional.empty();
+
+        return lore.stream().<Optional<List<CosmeticChroma>>>map(line -> {
+            var matcher = CHROMA_REGEX.matcher(line.getString());
+            if (!matcher.find()) return Optional.empty();
+
+            var chromaChars = matcher.group(1).chars().mapToObj(c -> (char)c);
+            var chromaList = chromaChars
+                    .map(CosmeticChroma::fromChar)
+                    .filter(Optional::isPresent).map(Optional::get)
+                    .toList();
+            return Optional.of(chromaList);
+        }).filter(Optional::isPresent).map(Optional::get).findFirst();
+    }
+
+    @Inject(
+            method = "isBarVisible",
+            at = @At("RETURN"),
+            cancellable = true
+    )
+    private void injectBarVisibility(CallbackInfoReturnable<Boolean> cir) {
+        if (IslandOptions.getCosmetics().isShowReputationBar()) {
+            this.getReputation().ifPresent(reputation -> cir.setReturnValue(true));
+        }
+        if (IslandOptions.getCosmetics().isShowChromaBar()) {
+            this.getChromas().ifPresent(chroma -> cir.setReturnValue(true));
+        }
+    }
+
+    @Inject(
+            method = "getBarWidth",
+            at = @At("RETURN"),
+            cancellable = true
+    )
+    private void injectBarWidth(CallbackInfoReturnable<Integer> cir) {
+        if (IslandOptions.getCosmetics().isShowReputationBar()) {
+            this.getReputation().ifPresent(reputation ->
+                    cir.setReturnValue(Math.min(Mth.mulAndTruncate(reputation, 13), 13))
+            );
+        }
+        if (IslandOptions.getCosmetics().isShowChromaBar()) {
+            this.getChromas().ifPresent(chromas ->
+                    cir.setReturnValue(Math.min(Mth.mulAndTruncate(CosmeticChroma.toFraction(chromas), 13), 13))
+            );
+        }
+    }
+
+    @Inject(
+            method = "getBarColor",
+            at = @At("RETURN"),
+            cancellable = true
+    )
+    private void injectBarColor(CallbackInfoReturnable<Integer> cir) {
+        if (IslandOptions.getCosmetics().isShowReputationBar()) {
+            if (this.getReputation().isPresent()) {
+                cir.setReturnValue(REPUTATION_BAR_COLOR);
+            }
+        }
+        if (IslandOptions.getCosmetics().isShowChromaBar()) {
+            this.getChromas().ifPresent(chromas -> {
+                if (chromas.isEmpty()) return;
+
+                var currentChromaIdx = (int) ((System.currentTimeMillis() / CHROMA_BAR_COLOR_SPEED) % chromas.size());
+                var nextChromaIdx = (currentChromaIdx + 1) % chromas.size();
+
+                var currentChromaColor = chromas.get(currentChromaIdx).toColor();
+                var nextChromaColor = chromas.get(nextChromaIdx).toColor();
+
+                var interpolatedProgress = (float) (System.currentTimeMillis() % CHROMA_BAR_COLOR_SPEED) / CHROMA_BAR_COLOR_SPEED;
+                var interpolatedColor = ARGB.lerp(interpolatedProgress, currentChromaColor, nextChromaColor);
+
+                cir.setReturnValue(interpolatedColor);
+            });
+        }
+    }
+}
