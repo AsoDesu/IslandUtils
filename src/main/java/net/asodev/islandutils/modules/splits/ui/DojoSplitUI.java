d8dfe6bf3e0c952a190846884465feac1bbcbce6
LukynkaCZE <jiraklukas058@seznam.cz>
2025-09-19 23:14:55 +0200

    Refactor how fonts and icons work

--- src/main/java/net/asodev/islandutils/modules/splits/ui/DojoSplitUI.java
+++ src/main/java/net/asodev/islandutils/modules/splits/ui/DojoSplitUI.java
@@ -7,7 +7,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.RenderPipelines;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
